2d39b3e17dd8347879b6b42f1542cee672aae04d
Aviu00 <93730715+Aviu00@users.noreply.github.com>
2025-11-17 16:02:48 +0000

    I hate this sprinting interaction (#5123)
    I hate this.

--- Content.Goobstation.Server/Sprinting/SprintingSystem.cs
+++ Content.Goobstation.Server/Sprinting/SprintingSystem.cs
@@ -4,9 +4,43 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Goobstation.Common.Standing;
 using Content.Goobstation.Shared.Sprinting;
 
 namespace Content.Goobstation.Server.Sprinting;
 
 public sealed class SprintingSystem : SharedSprintingSystem
-{}
+{
+
+    [Dependency] private readonly StunSystem _stunSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<SprinterComponent, StartCollideEvent>(OnCollide);
+    }
+
+    private void OnCollide(EntityUid uid, SprinterComponent sprinter, ref StartCollideEvent args)
+    {
+        var otherUid = args.OtherEntity;
+
+        if (uid.Id < otherUid.Id)
+            return;
+
+        if (!sprinter.IsSprinting)
+        {
+            return;
+        }
+
+        if (!TryComp(otherUid, out SprinterComponent? otherSprinter) || !otherSprinter.IsSprinting)
+        {
+            return;
+        }
+
+        _stunSystem.TryKnockdown(uid, sprinter.KnockdownDurationOnInterrupt, false, DropHeldItemsBehavior.NoDrop);
+        _stunSystem.TryKnockdown(otherUid,
+            otherSprinter.KnockdownDurationOnInterrupt,
+            false,
+            DropHeldItemsBehavior.NoDrop);
+    }
+}
