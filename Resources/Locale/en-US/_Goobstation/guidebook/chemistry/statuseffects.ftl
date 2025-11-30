madeline-celeste/Maidstation

[bugfix] profanol effect description (#5092)

Resources/Locale/en-US/guidebook/chemistry/statuseffects.ftl
@@ -21,3 +21,4 @@ reagent-effect-status-effect-StaminaModifier = modified stamina
 reagent-effect-status-effect-RadiationProtection = radiation protection
 reagent-effect-status-effect-Drowsiness = drowsiness
 reagent-effect-status-effect-Adrenaline = adrenaline
+reagent-effect-status-effect-Vulgarity = vulgar

catban132/Funny-Station

Resources/Locale/en-US/_Goobstation/guidebook/chemistry/statuseffects.ftl
@@ -1,4 +1,6 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-reagent-effect-status-effect-Dementia = dementia
+entity-effect-status-effect-Dementia = dementia
+
+entity-effect-status-effect-Vulgarity = vulgar